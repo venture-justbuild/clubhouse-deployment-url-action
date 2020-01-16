90da53e4ac9bc6e8848e6345d095d5f6f9650df4
Oliver Spindler <os@oliverspindler.com>
2020-01-16 17:04:03 +0100

    Add debug output

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import core from '@actions/core'
+import github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    core.debug(payload)
+    core.setOutput('payload', payload)
   } catch (error) {
     core.setFailed(error.message)
   }
